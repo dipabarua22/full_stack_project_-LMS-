dc06a6519a099f34e87da320c27442da99ddd279
dipa <dipabarua44@gmail.com>
2025-12-26 23:33:39 +0600

    feat:add something

--- lms_backend/users/views.py
+++ lms_backend/users/views.py
@@ -22,3 +22,10 @@ def user_list_create(request):
 
         serializer = UserSerializer(users, many = True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors,status =400)
